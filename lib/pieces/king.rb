2943860b5181b6c857b9cd50598fa3fe88f34619
ellielle <40385743+ellielle@users.noreply.github.com>
2019-02-15 10:33:32 -0500

    add basic functionality to chess pieces

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,5 +1,12 @@
 require "board"
 
 class King < Piece
+  attr_accessor :moves, :position, :icon
+
+  def initialize(position, is_first)
+    @moves = []
+    @position = position
+    @icon = is_first ? "♔" : "♚"
+  end
 
 end
--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -1,5 +1,11 @@
 require "board"
 
 class Queen < Piece
+  attr_accessor :moves, :position, :icon
 
+  def initialize(position, is_first)
+    @moves = []
+    @position = position
+    @icon = is_first ? "♕" : "♛"
+  end
 end
