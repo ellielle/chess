2a7e60c7929dd3bd2b7fa44f072a6e5c8d989803
ellielle <40385743+ellielle@users.noreply.github.com>
2019-03-18 05:08:20 -0400

    deprecate prev! string method

--- lib/string.rb
+++ lib/string.rb
@@ -7,7 +7,7 @@ class String
     "\e[35m#{self}\e[0m"
   end
 
-  def prev!
+  def prev! #no longer being used, kept in for my personal future reference
     self[0] = (self.ord - 1).chr
   end
 end
