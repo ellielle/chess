594c4f6a552e81543dee773765b12dc8487f1914
ellielle <40385743+ellielle@users.noreply.github.com>
2019-03-26 06:27:04 -0400

    add method to find a piece's potential moves

--- lib/piece_methods.rb
+++ lib/piece_methods.rb
@@ -9,7 +9,24 @@ module PieceMethods
     end
   end
 
-  def potential_moves(moves, position, is_white)
-
+  def potential_moves(moves, position, is_white, board_state)
+    possible_moves = {}
+    moves.each do |move|
+      pos = [move[0] + position[0], move[1] + position[1]]
+      next if pos[0] <= 0
+      converted_pos = convert_number_to_position([pos[0], pos[1]]).to_sym
+      next unless board_state.key?(converted_pos)
+      next unless self.path_clear?([position, pos], board_state)
+      if board_state[converted_pos].nil?
+        possible_moves[pos] = board_state[converted_pos]
+      elsif is_white
+        next if board_state[converted_pos].is_white
+        possible_moves[pos] = board_state[converted_pos]
+      elsif !is_white
+        next unless board_state[converted_pos].is_white
+        possible_moves[pos] = board_state[converted_pos]
+      end
+    end
+    possible_moves
   end
 end
