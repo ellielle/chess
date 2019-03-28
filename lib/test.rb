9f10c2f10847d69a94dce6382b3397601d7ceeed
ellielle <40385743+ellielle@users.noreply.github.com>
2019-03-28 06:31:40 -0400

    change string.to_sym to just symbols

--- lib/test.rb
+++
@@ -1,5 +0,0 @@
-require_relative '../lib/board'
-
-test = Board.new("Ellie", "Canada")
-test.display
-test.move_piece()
--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -6,30 +6,30 @@ describe Pawn do
   describe "initializing to correct color" do
     context "when first player" do
       it "creates white pawn piece" do
-        expect(board_state["a2".to_sym].icon).to eql("P".white)
+        expect(board_state[:a2].icon).to eql("P".white)
       end
     end
   end
   describe "#in_moveset?" do
     context "when chosen move is 1 space forward" do
       it "returns true" do
-        expect(board_state["a2".to_sym].in_moveset?([[2,1], [2,2]], board_state)).to be_truthy
+        expect(board_state[:a2].in_moveset?([[2,1], [2,2]], board_state)).to be_truthy
       end
     end
     context "when chosen move is 2 spaces forward and it's first move" do
       it "returns true" do
-        expect(board_state["a2".to_sym].in_moveset?([[2,1], [2,3]], board_state)).to be_truthy
+        expect(board_state[:a2].in_moveset?([[2,1], [2,3]], board_state)).to be_truthy
       end
     end
     context "when chosen move is 2 spaces forward and it's not first move" do
       it "returns false" do
-        board_state["a2".to_sym].instance_variable_set(:@first_move, false)
-        expect(board_state["a2".to_sym].in_moveset?([[2,1], [2,3]], board_state)).to be_falsey
+        board_state[:a2].instance_variable_set(:@first_move, false)
+        expect(board_state[:a2].in_moveset?([[2,1], [2,3]], board_state)).to be_falsey
       end
     end
     context "when chosen move is 3 spaces forward" do
       it "returns false" do
-        expect(board_state["a2".to_sym].in_moveset?([[2,1], [2,4]], board_state)).to be_falsey
+        expect(board_state[:a2].in_moveset?([[2,1], [2,4]], board_state)).to be_falsey
       end
     end
   end
