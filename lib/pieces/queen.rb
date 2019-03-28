ad3800f5622db2745fb980fb0511d898bfbe3694
ellielle <40385743+ellielle@users.noreply.github.com>
2019-03-28 05:21:38 -0400

    add #create_moveset instead of hard-coding moveset improperly

---
+++ lib/pieces/queen.rb
@@ -0,0 +1,36 @@
+class Queen
+  attr_accessor :position
+  attr_reader :moves, :icon, :is_white
+
+  def initialize(position, is_white)
+    @moves = create_moveset
+    @position = position
+    @icon = is_white ? "Q".white : "Q".black
+    @is_white = is_white
+    @potential_moves = nil
+  end
+
+  def in_moveset?(move, board_state)
+    #TODO change moveset like rook.rb
+
+    #TODO make sure diagonal moves are the same number or invalid move
+    #TODO ensure no pieces are in path except potentially at the finish pos
+  end
+
+  def create_moveset
+    moves = []
+    1.upto(7) { |num| moves << [0, num] }
+    1.upto(7) { |num| moves << [num, 0] }
+    1.upto(7) { |num| moves << [num, num] }
+    1.upto(7) { |num| moves << [num, -(num.abs)] }
+    1.upto(7) { |num| moves << [-(num.abs), num] }
+    -1.downto(-7) { |num| moves << [0, num] }
+    -1.downto(-7) { |num| moves << [num, 0] }
+    -1.downto(-7) { |num| moves << [num, num] }
+    moves
+  end
+
+  def find_potential_moves(board_state)
+    @potential_moves = potential_moves(@moves, @position, @is_white, board_state)
+  end
+end
