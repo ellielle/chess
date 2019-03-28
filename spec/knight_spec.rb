cc9515fa710b58517c8ea19a91827cbfbc897593
ellielle <40385743+ellielle@users.noreply.github.com>
2019-03-28 06:32:01 -0400

    add more tests for Knight pieces

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -6,19 +6,30 @@ describe Knight do
   describe "initializing to correct color" do
     context "when first player" do
       it "creates white knight piece" do
-        expect(board_state["b1".to_sym].icon).to eql("K".white)
+        expect(board_state[:b1].icon).to eql("K".white)
       end
     end
   end
   describe "#in_moveset?" do
     context "when chosen move is allowed" do
       it "returns true" do
-        expect(board_state["b1".to_sym].in_moveset?([[2,1], [3,3]], board_state)).to be_truthy
+        expect(board_state[:b1].in_moveset?([[2,1], [3,3]], board_state)).to be_truthy
       end
     end
-    context "when chose move isn't allowed" do
+    context "when chosen move isn't allowed" do
       it "returns false" do
-        expect(board_state["b1".to_sym].in_moveset?([[2,1], [7,4]], board_state)).to be_falsey
+        expect(board_state[:b1].in_moveset?([[2,1], [7,4]], board_state)).to be_falsey
+      end
+    end
+    context "when chosen space already has a piece" do
+      it "returns false" do
+        expect(board_state[:b1].in_moveset?([[2,1], [4,2]], board_state)).to be_falsey
+      end
+    end
+    context "when chosen space already has a piece of the opposite color" do
+      it "returns true" do
+        board_state[:d2] = board_state[:b8]
+        expect(board_state[:b1].in_moveset?([[2,1], [4,2]], board_state)).to be_truthy
       end
     end
   end
