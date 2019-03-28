44ee6cb408902c3f3e562035f184d416d4c45e4c
ellielle <40385743+ellielle@users.noreply.github.com>
2019-03-28 06:16:59 -0400

    fix typo in tests

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -1,7 +1,7 @@
 require "pieces/rook"
 
 describe Rook do
-  subject(:board) { Board.new("Ellie", "Canada") }
+  subject(:board) { Board.new("p1", "p2") }
   let(:board_state) { subject.instance_variable_get(:@board_state) }
   describe "#in_moveset?" do
     context "when moving up the board as a white piece" do
@@ -31,7 +31,7 @@ describe Rook do
         expect(board_state[:a1].in_moveset?([[2,2], [6,2]], board_state)).to be_falsey
       end
     end
-    context "when the landing space has the other place's piece" do
+    context "when the landing space has the other player's piece" do
       it "returns true" do
         expect(board_state[:a1].in_moveset?([[1,3], [1,7]], board_state)).to be_truthy
         expect(board_state[:a8].in_moveset?([[1,7], [1,3]], board_state)).to be_truthy
