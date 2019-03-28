f4d4c751dca6b36684bb6aa4832c6d0d36213067
ellielle <40385743+ellielle@users.noreply.github.com>
2019-03-28 06:17:30 -0400

    add #in_moveset? and #path_clear? to King pieces

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -17,11 +17,27 @@ class King
   end
 
   def in_moveset?(move, board_state)
-
+    @moves.each do |moveset|
+      if move[0][0] + moveset[0] == move[1][0] && move[0][1] + moveset[1] == move[1][1] &&
+          path_clear?(move[1], board_state)
+        return true
+      end
+    end
+    false
     #TODO check for check & checkmate before allowing move
     # each time a piece moves, iterate to make an array of all possible moves
     # check if any of them is_a?(King) and probably check color
     # no need to build list until pieces start moving
+  end
+
+  def path_clear?(move, board_state)
+    move_to_sym = convert_number_to_position(move).to_sym
+    if @is_white
+      return true if board_state[move_to_sym].nil? || !board_state[move_to_sym].is_white
+    else
+      return true if board_state[move_to_sym].nil? || board_state[move_to_sym].is_white
+    end
+    false
   end
 
   def find_potential_moves(board_state)
