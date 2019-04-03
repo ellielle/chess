f89bf677e5fee16bfcb0d2fbbaaff5b72a4299e8
ellielle <40385743+ellielle@users.noreply.github.com>
2019-04-03 05:52:55 -0400

    move #path_clear code to module file to be used with Queen pieces

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -27,59 +27,7 @@ class Rook
   end
 
   def path_clear?(move, board_state)
-    if move[0][0] == move[1][0]
-      if move[0][1] < move[1][1]
-        start = move[0][1] + 1
-        start.upto(move[1][1]) do |space|
-          if space == move[1][1] && is_white
-            return false if landing_piece_is_white?(@is_white, move[0][0], space, board_state)
-            return true
-          elsif space == move[1][1] && !is_white
-            return false unless landing_piece_is_white?(@is_white, move[0][0], space, board_state)
-            return true
-          end
-          return false unless board_state[convert_number_to_position([move[0][0], space]).to_sym].nil?
-        end
-      elsif move[0][1] > move[1][1]
-        start = move[0][1] - 1
-        start.downto(move[1][1]) do |space|
-          if space == move[1][1] && is_white
-            return false if landing_piece_is_white?(@is_white, move[0][0], space, board_state)
-            return true
-          elsif space == move[1][1] && !is_white
-            return false unless landing_piece_is_white?(@is_white, move[0][0], space, board_state)
-            return true
-          end
-          return false unless board_state[convert_number_to_position([move[0][0], space]).to_sym].nil?
-        end
-      end
-    elsif move[0][1] == move[1][1]
-      if move[0][0] < move[1][0]
-        start = move[0][0] + 1
-        start.upto(move[1][0]) do |space|
-          if space == move[1][0] && is_white
-            return false if landing_piece_is_white?(@is_white, space, move[0][1], board_state)
-            return true
-          elsif space == move[1][0] && !is_white
-            return false unless landing_piece_is_white?(@is_white, space, move[0][1], board_state)
-            return true
-          end
-          return false unless board_state[convert_number_to_position([space, move[0][1]]).to_sym].nil?
-        end
-      elsif move[0][0] > move[1][0]
-        start = move[0][0] - 1
-        start.downto(move[1][0]) do |space|
-          if space == move[1][0] && is_white
-            return false if landing_piece_is_white?(@is_white, space, move[0][1], board_state)
-            return true
-          elsif space == move[1][0] && !is_white
-            return false unless landing_piece_is_white?(@is_white, space, move[0][1], board_state)
-            return true
-          end
-          return false unless board_state[convert_number_to_position([space, move[0][1]]).to_sym].nil?
-        end
-      end
-    end
+    horizontal_path_clear?(move, board_state)
   end
 
   def create_moveset
