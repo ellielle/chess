1d75949854a81e7c3c8c7d45dc2bf6ad9cd19104
ellielle <40385743+ellielle@users.noreply.github.com>
2019-04-03 01:44:53 -0400

    fix issues with black pieces slipping through

--- lib/piece_methods.rb
+++ lib/piece_methods.rb
@@ -4,7 +4,8 @@ module PieceMethods
     unless board_state[pos].nil?
       return true if board_state[pos].is_white
     end
-    false
+    return false if is_white
+    return true if !is_white
   end
 
   def potential_moves(moves, position, is_white, board_state)
@@ -12,7 +13,8 @@ module PieceMethods
     possible_moves = {}
     moves.each do |move|
       pos = [move[0] + position[0], move[1] + position[1]]
-      next if pos[0] <= 0
+      next if pos[0] <= 0 || pos[0] > 8
+      next if pos[1] <= 0 || pos[1] > 8
       converted_pos = convert_number_to_position([pos[0], pos[1]]).to_sym
       next unless board_state.key?(converted_pos)
       next unless self.path_clear?([position, pos], board_state)
