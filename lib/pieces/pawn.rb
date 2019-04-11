ff58bcb4219c11880511e47e1cd1c369bd0d3b4f
ellielle <40385743+ellielle@users.noreply.github.com>
2019-04-11 05:51:43 -0400

    refactor Pawn to work properly with Board#move_piece

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -6,7 +6,7 @@ class Pawn
   include ConvertCoordinates, PieceMethods
 
   attr_accessor :position
-  attr_reader :moves, :icon, :is_white, :potential_moves
+  attr_reader :moves, :icon, :is_white, :potential_moves, :can_be_en_passant
 
   def initialize(position, is_white)
     @moves = is_white ? white_moves : black_moves
@@ -21,67 +21,45 @@ class Pawn
   end
 
   def white_moves
-    hsh = { normal: [0,1],
-            two: [0,2],
-            en_passant: [[1,1],[1,-1]],
-            take: [[1,1],[1,-1]]
-    }
+    moves = [[0, 1], [0, 2], [1, 1], [-1, 1]]
   end
 
   def black_moves
-    hsh = { normal: [0,-1],
-            two: [0,-2],
-            en_passant: [[-1,-1],[-1,1]],
-            take: [[-1,-1],[-1,1]]
-    }
+    moves = [[0, -1], [0, -2], [-1, -1], [1, -1]]
   end
 #TODO unset enpassant on next move if it's set to true
   def in_moveset?(move, board_state)
-    start = convert_number_to_position(move[0])
     finish = convert_number_to_position(move[1])
+    return false if move[1][0] < 1 || move[1][0] > 8 || move[1][1] < 0 || move[1][1] > 8
     if @is_white
-      @moves.each do |move_type, moveset|
-        if move_type == :normal
-          if move[0][0] + moveset[0] == move[1][0] && move[0][1] + moveset[1] == move[1][1]
-            @first_move = false if @first_move
-            @can_be_en_passant = false if @can_be_en_passant
-            return true
-          end
-        elsif move_type == :two && @first_move
-          if move[0][0] + moveset[0] == move[1][0] && move[0][1] + moveset[1] == move[1][1]
-            @first_move = false
+      @moves.each do |moveset|
+        if move[0][0] + moveset[0] == move[1][0] && move[0][1] + moveset[1] == move[1][1]
+          if moveset == [0, 1]
+            return true if board_state[finish.to_sym].nil?
+          elsif moveset == [0, 2]
+            return false unless @first_move
             @can_be_en_passant = true
             return true
-          end
-        elsif move_type == :take
-          moveset.each do |m|
-            if move[0][0] + m[0] == move[1][0] && move[0][1] + m[1] == move[1][1]
-              @first_move = false if @first_move
-              @can_be_en_passant = false if @can_be_en_passant
+          elsif moveset == [1, 1] || moveset == [-1, 1]
+            unless board_state[finish.to_sym].nil? || board_state[finish.to_sym].is_white
               return true
             end
           end
         end
       end
+      @first_move = false if @first_move
+      @can_be_en_passant = false if @can_be_en_passant
     else
-      @moves.each do |move_type, moveset|
-        if move_type == :normal
-          if move[0][0] + moveset[0] == move[1][0] && move[0][1] + moveset[1] == move[1][1]
-            @first_move = false if @first_move
-            @can_be_en_passant = false if @can_be_en_passant
-            return true
-          end
-        elsif move_type == :two && @first_move
-          if move[0][0] + moveset[0] == move[1][0] && move[0][1] + moveset[1] == move[1][1]
-            @first_move = false if @first_move
+      @moves.each do |moveset|
+        if move[0][0] + moveset[0] == move[1][0] && move[0][1] + moveset[1] == move[1][1]
+          if moveset == [0, -1]
+            return true if board_state[finish.to_sym].nil?
+          elsif moveset == [0, -2]
+            return false unless @first_move
             @can_be_en_passant = true
             return true
-          end
-        elsif move_type == :take
-          moveset.each do |m|
-            if move[0][0] + m[0] == move[1][0] && move[0][1] + m[1] == move[1][1]
-              @first_move = false if @first_move
-              @can_be_en_passant = false if @can_be_en_passant
+          elsif moveset == [-1, -1] || moveset == [1, -1]
+            unless board_state[finish.to_sym].nil? || !board_state[finish.to_sym].is_white
               return true
             end
           end
