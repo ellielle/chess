eec38494714efd931fbef27d3e7b53420d9a1e72
ellielle <40385743+ellielle@users.noreply.github.com>
2019-04-16 02:42:16 -0400

    add functionality to load a previous game save

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,27 @@
 require_relative '../lib/chess'
 
-run_game = Chess.new
+def load_game
+  if File.exist?("../saves/chess.yaml") &&
+      File.file?("../saves/chess.yaml")
+    save = YAML.load_file("../saves/chess.yaml")
+    game = save
+    game.game_loop
+  else
+    puts "There are no saved games."
+    start_game
+  end
+end
+
+def start_game
+  puts "Would you like to load your saved game? (Yn)"
+  input = gets.chomp.downcase
+  case input
+  when "y" then load_game
+  when "n" then game = Chess.new
+  else
+    puts "Invalid input."
+    start_game
+  end
+end
+
+start_game
