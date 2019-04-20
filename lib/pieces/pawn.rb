7bdcb86fcd96c794d035845dbc6a3bb94f72e120
ellielle <40385743+ellielle@users.noreply.github.com>
2019-04-20 04:55:33 -0400

    fix bug with pawn preventing a move if first move was invalid

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -27,42 +27,47 @@ class Pawn
   end
 
   def in_moveset?(move, board_state)
+    valid = false
     finish = convert_number_to_position(move[1])
     return false if move[1][0] < 1 || move[1][0] > 8 || move[1][1] < 0 || move[1][1] > 8
     if @is_white
       @moves.each do |moveset|
         if move[0][0] + moveset[0] == move[1][0] && move[0][1] + moveset[1] == move[1][1]
           if moveset == [0, 1]
-            return true if board_state[finish.to_sym].nil?
+            valid = true if board_state[finish.to_sym].nil?
           elsif moveset == [0, 2]
             return false unless @first_move
             @can_be_en_passant = true
-            return true
+            valid = true
           elsif moveset == [1, 1] || moveset == [-1, 1]
             unless board_state[finish.to_sym].nil? || board_state[finish.to_sym].is_white
-              return true
+              valid = true
             end
           end
         end
       end
-      @first_move = false if @first_move
+      @first_move = false if @first_move && valid
       @can_be_en_passant = false if @can_be_en_passant
+      return true if valid
     else
       @moves.each do |moveset|
         if move[0][0] + moveset[0] == move[1][0] && move[0][1] + moveset[1] == move[1][1]
           if moveset == [0, -1]
-            return true if board_state[finish.to_sym].nil?
+            valid = true if board_state[finish.to_sym].nil?
           elsif moveset == [0, -2]
             return false unless @first_move
             @can_be_en_passant = true
-            return true
+            valid = true
           elsif moveset == [-1, -1] || moveset == [1, -1]
             unless board_state[finish.to_sym].nil? || !board_state[finish.to_sym].is_white
-              return true
+              valid = true
             end
           end
         end
       end
+      @first_move = false if @first_move && valid
+      @can_be_en_passant = false if @can_be_en_passant
+      return true if valid
     end
     false
   end
